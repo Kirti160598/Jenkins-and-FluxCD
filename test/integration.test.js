8e1b930b79a2d52397e5488ce9709d71e04a5e52
Kirti  She/her <72144141+Kirti160598@users.noreply.github.com>
2025-05-05 13:39:32 +0530

    Updated integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app } = require('../../app');
+const { app } = require('../app.js');
 
 describe('Integration test GET /greet/:name', () => {
   test('responds with greeting message', async () => {
